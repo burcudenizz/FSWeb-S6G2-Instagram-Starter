b5afdadc4e15b2bc1eba598436252caabb7e1934
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-03-28 20:22:10 +0300

    sayfa oluştu. beğen.js ve yorum.js tamamlanmadı.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from "react";
+import Gönderiler from "./bileşenler/Gönderiler/Gönderiler.js";
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu.js";
+import sahteVeri from "./sahte-veri.js";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+import "./App.css";
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [arama, setArama] = useState("");
+  const gonderiyiBegen = (gonderiID) => {
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -28,10 +32,21 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
+
+    setGonderiler(
+      gonderiler.map((item) => {
+        if (item.id === gonderiID) {
+          item.likes++;
+        }
+        return item;
+      })
+    );
   };
 
   return (
-    <div className='App'>
+    <div className="App">
+      <AramaÇubuğu />
+      <Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} />
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
